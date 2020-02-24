a98781cf6870a5d492246d51ff4cbae895baf145
polanco15 <araidepolanco@gmail,com>
2020-02-24 18:50:35 -0400

    Commit
    Subir el proyeto x2

---
+++ tarea4.3/tarea4.3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tarea4._3.Startup))]
+namespace tarea4._3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
