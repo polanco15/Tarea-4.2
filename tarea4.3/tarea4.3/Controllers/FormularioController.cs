7cc03ae436a22e57bdbb22a663a5776279e1a261
polanco15 <araidepolanco@gmail,com>
2020-02-27 18:18:08 -0400

    Actualizacion
    Actualizacion de vistas y los datos faltantes al formulario

--- tarea4.3/tarea4.3/Controllers/FormularioController.cs
+++ tarea4.3/tarea4.3/Controllers/FormularioController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tarea4._3.Models;
+using System.IO;
 
 
 namespace tarea4._3.Controllers
@@ -14,9 +16,34 @@ namespace tarea4._3.Controllers
         {
             return View();
         }
-        public ActionResult Convertidor ()
+        [HttpPost]
+        public ActionResult  Resultado(Formulario forr)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (forr.Foto != null && forr.Curriculum != null)
+                {
+
+
+                    string nombreFoto = "foto_" + DateTime.Now.ToString("ddMMyyHHmmss") +
+                        Path.GetExtension(forr.Foto.FileName);
+                    forr.Foto.SaveAs(Path.Combine(Server.MapPath("~/Fotos/"), nombreFoto));
+
+                    ViewBag.foto = nombreFoto;
+
+                    string nombreCurri = "curriculum_" + DateTime.Now.ToString("ddMMyyHHmmss") +
+                        Path.GetExtension(forr.Curriculum.FileName);
+                    forr.Curriculum.SaveAs(Path.Combine(Server.MapPath("~/Curriculums/"), nombreCurri));
+                    ViewBag.curri = nombreCurri;
+                    ViewBag.StatusCurri = "Currículum Enviado";
+                    return View(forr);
+                }
+
+            }
+            ViewBag.StatusFoto = "Error al enviar la foto";
+            ViewBag.StatusCurri = "Error al enviar el currículum";
+            return View(forr);
+
         }
     }
 }
--- tarea4.3/tarea4.3/Models/Formulario.cs
+++ tarea4.3/tarea4.3/Models/Formulario.cs
@@ -2,20 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace tarea4._3.Models
 {
     public class Formulario
     {
-        public int Cedula;
-        public string Nombre;
-        public string Apellido;
-        public int Edad;
-        public long Telefono;
-        public string Correo;
-        public string Genero;
-        public string puesto;
-        
+        [Required(ErrorMessage = "La cedula es obligatoria")]
+        public long  Cedula { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        public string Apellido { get; set; }
+        [Range(16, 99)]
+        public int Edad { get; set; }
+
+        public long Telefono { get; set; }
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+            ErrorMessage = "Dirección de Correo electrónico incorrecta.")]
+        public string Correo { get; set; }
+        public string Genero { get; set; }
+        public string Puesto { get; set; }
+        public HttpPostedFileBase foto { get; set; }
+        public HttpPostedFileBase curriculum { get; set; }
+    }
+    public enum Sexo
+    {
+        Masculino,
+        Femenino
+    }
+    public enum puesto
+    {
+        Director_del_hotel,
+        Asistente_director_general,
+        Director_de_personal,
+        Director_contabilidad,
+        Director_Marketing,
+        Director_ingenieria,
+        Director_compras,
+        Director_Restaurante,
+        Jefe_Reservas,
+        Jefe_eventos,
+        Encagardas_Limpieza,
+        Jefe_recepcion,
+        Encargado_seguridad,
+        Jefe_gimnasio,
+        Jefe_servicio_Telefononico,
+        Chef,
+        Cocineros,
+        Lavaplatos,
+        Camareros,
+        Musicos_Animadores
+
     }
 }
